b60ffa9d229e8a96cc36084677481012a876c007
Cledson Vilanculo <cledsonvilanculo4@gmail.com>
2026-03-01 18:26:45 +0200

    Change MySQL connection parameters in Conexao.java
    Updated database connection details for MySQL.

--- javatodo/src/main/java/com/cledson/Conexao.java
+++ javatodo/src/main/java/com/cledson/Conexao.java
@@ -5,9 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-    private static final String url = "jdbc:mysql://localhost:3306/listasdetarefas?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
-    private static final String user = "root";
-    private static final String password = "minhasenha123@";
+    // sim eu sei que isto nao deveria estar visivel para todo mundo, mas tanto faz isto e so um proejeto simples que fiz para aprender MySQL
+    private static final String url = "jdbc:mysql://93qc9n.h.filess.io:3307/javatodo_havingoff?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
+    private static final String user = "javatodo_havingoff";
+    private static final String password = "286278c0daf2f9603ddba279e4cf6115d6c718f5";
 
     public static Connection connect() {
         try {
